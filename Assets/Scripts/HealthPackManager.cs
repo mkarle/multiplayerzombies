b95b27d982cea50008af438289346760be506a64
Royce Mou <Royce Mou>
2016-11-16 08:13:31 -0800

    Added Initial Health Pack Scripts

---
+++ Assets/Scripts/HealthPackManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class HealthPackManager : NetworkBehaviour {
+
+	public int restoreAmount;
+
+	public override void OnStartServer() {
+		
+	}
+
+	// TODO: how do we know when the health hack is hit?
+	public void OnHit() {
+		if (isLocalPlayer) {
+			// TODO: how do we get the local player object?
+//			playerHealth.RestoreHealth (restoreAmount);
+		}
+		// restores health to players by looking at the health pack and clicking the trigger
+	}
+	// TODO: maybe instead of a fixed number of health packs, we want to spawn them slowly over time?
+}
---
+++ Assets/Scripts/SpawnHealthPack.cs
@@ -0,0 +1,28 @@
+﻿// Modified from: https://unity3d.com/learn/tutorials/topics/multiplayer-networking/handling-non-player-objects?playlist=29690
+
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnHealthPack : NetworkBehaviour {
+
+	public GameObject healthPack;
+	public int numberOfHealthPacks;
+
+	public override void OnStartServer() {
+		for (int i = 0; i < numberOfHealthPacks; i++) {
+			var spawnPosition = new Vector3(
+				Random.Range(-8.0f, 8.0f),
+				0.0f,
+				Random.Range(-8.0f, 8.0f));
+
+			var spawnRotation = Quaternion.Euler( 
+				0.0f, 
+				Random.Range(0,180), 
+				0.0f);
+
+			var enemy = (GameObject) Instantiate(healthPack, spawnPosition, spawnRotation);
+			NetworkServer.Spawn(enemy);
+		}
+	}
+	// TODO: maybe instead of a fixed number of health packs, we want to spawn them slowly over time?
+}
