9ca1baaa9653510ebfa16b6c6a4ff668c4ae4b39
Lauren E. Buck <lauren.e.buck.1@vanderbilt.edu>
2016-12-05 12:44:37 -0600

    added assets/menu changes

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour {
+
+	public void LoadByIndex(int index)
+    {
+
+        SceneManager.LoadScene(index);
+
+    }
+}
