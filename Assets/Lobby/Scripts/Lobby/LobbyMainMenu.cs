c7ec91f5c12e85cce4016d464eae7684ffc5d597
mangodingo <markakarle@vanderbilt.edu>
2016-12-06 21:52:22 -0600

    gave knife a collider, fixed zombie navigation, added a lobby manager to menu scene (still needs work)

---
+++ Assets/Lobby/Scripts/Lobby/LobbyMainMenu.cs
@@ -0,0 +1,92 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+namespace Prototype.NetworkLobby
+{
+    //Main menu, mainly only a bunch of callback called by the UI (setup throught the Inspector)
+    public class LobbyMainMenu : MonoBehaviour 
+    {
+        public LobbyManager lobbyManager;
+
+        public RectTransform lobbyServerList;
+        public RectTransform lobbyPanel;
+        
+
+        public static int gameID = 0;
+        public void OnEnable()
+        { 
+            
+            
+        }
+
+        public void OnClickHost()
+        {
+            lobbyManager.StartHost();
+        }
+
+        public void OnClickJoin()
+        {
+            lobbyManager.ChangeTo(lobbyPanel);
+
+            lobbyManager.networkAddress = "GAME" + gameID;
+            lobbyManager.StartClient();
+
+            lobbyManager.backDelegate = lobbyManager.StopClientClbk;
+            lobbyManager.DisplayIsConnecting();
+
+            lobbyManager.SetServerInfo("Connecting...", lobbyManager.networkAddress);
+        }
+
+        public void OnClickDedicated()
+        {
+            lobbyManager.ChangeTo(null);
+            lobbyManager.StartServer();
+
+            lobbyManager.backDelegate = lobbyManager.StopServerClbk;
+
+            lobbyManager.SetServerInfo("Dedicated Server", lobbyManager.networkAddress);
+        }
+
+        public void OnClickCreateMatchmakingGame()
+        {
+            lobbyManager.StartMatchMaker();
+            lobbyManager.matchMaker.CreateMatch(
+                "GAME" + gameID,
+                (uint)lobbyManager.maxPlayers,
+                true,
+				"", "", "", 0, 0,
+				lobbyManager.OnMatchCreate);
+            gameID++;
+            lobbyManager.backDelegate = lobbyManager.StopHost;
+            lobbyManager._isMatchmaking = true;
+            lobbyManager.DisplayIsConnecting();
+
+            lobbyManager.SetServerInfo("Matchmaker Host", lobbyManager.matchHost);
+        }
+
+        public void OnClickOpenServerList()
+        {
+            lobbyManager.StartMatchMaker();
+            lobbyManager.backDelegate = lobbyManager.SimpleBackClbk;
+            lobbyManager.ChangeTo(lobbyServerList);
+        }
+
+        void onEndEditIP(string text)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                OnClickJoin();
+            }
+        }
+
+        void onEndEditGameName(string text)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                OnClickCreateMatchmakingGame();
+            }
+        }
+
+    }
+}
