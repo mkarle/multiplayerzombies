60d833d0a595c6f5b46bf7f08f73bde9613f8954
GeoffRen <geoff.ren@vanderbilt.edu>
2016-12-13 16:19:32 -0600

    trying to fix the cameras ingame

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -7,21 +7,38 @@ public class PlayerSetup : NetworkBehaviour
 
 	[SerializeField]
 	Behaviour[] nonLocalComponentsToDisable;
-
+	bool disabled = false;
 	// Use this for initialization
 	void Start () 
 	{
-		if (!isLocalPlayer) {
-			for (int i = 0; i < nonLocalComponentsToDisable.Length; i++) {
-				nonLocalComponentsToDisable [i].enabled = false;
-			}
+		
+	}
+	void Update (){
+		if (!disabled) {
+			if (!isLocalPlayer) {
+				for (int i = 0; i < nonLocalComponentsToDisable.Length; i++) {
+					if (nonLocalComponentsToDisable [i] == null) {
+						
+						disabled = false;
+						break;
+					}
+					else
+						nonLocalComponentsToDisable [i].enabled = false;
+				}
+				disabled = true;
 
-		} else 
-		{
-            GameManager.numPlayers++;
-			Camera.main.gameObject.SetActive (false);
-			for (int i = 0; i < nonLocalComponentsToDisable.Length; i++) {
-				nonLocalComponentsToDisable [i].enabled = true;
+			} else {
+				GameManager.numPlayers++;
+				Camera.main.gameObject.SetActive (false);
+				for (int i = 0; i < nonLocalComponentsToDisable.Length; i++) {
+					if (nonLocalComponentsToDisable [i] == null) {
+						disabled = false;
+						break;
+					}
+					else
+						nonLocalComponentsToDisable [i].enabled = true;
+				}
+				disabled = true;
 			}
 		}
 	}
